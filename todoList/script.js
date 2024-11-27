996a9b3b3b57529f440faed78807c5b896abf625
Daniel <daniel.mg1705@gmail.com>
2024-11-27 20:31:01 +0100

    añadir archivos

--- todoList/script.js
+++ todoList/script.js
@@ -3,8 +3,42 @@ let completadas = [];
 let pendientes = [];
 
 let input = $('#inputTarea');
-let botonAñadir = $('#botonAñadir')
+let botonAñadir = $('#botonAñadir');
+let formulario = $('#formulario');
+let mensajeError = $('#error');
+let tablaTareas = $('#tablaTareas');
+
+const tarea = $('#inputTarea');
+
 
 function agregar() {
-    
+    formulario.on('submit', function(event) {
+        event.preventDefault(); 
+
+        
+        if (tarea.val().trim() === '') { 
+            mensajeError.removeClass('d-none');  
+        } else {
+            mensajeError.addClass('d-none');
+            let tareaText = tarea.val().trim(); 
+            todas.push(tareaText); 
+            pendientes.push(tareaText);
+            input.val(''); 
+            actualizarTabla(tareaText);
+        }
+    });
 }
+
+
+function actualizarTabla(tareaText) {
+    
+    if (tareaText !== '') {
+        let nuevaFila = `<tr><td>${tareaText}</td></tr>`; 
+
+        
+        tablaTareas.append(nuevaFila);
+    }
+}
+
+
+agregar();
