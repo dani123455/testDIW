e58df8ff10218f203ad165c8c8da7a76f92c9bff
dani123455 <147657091+dani123455@users.noreply.github.com>
2024-11-28 14:16:52 +0100

    añadiendo archivos

--- todoList/script.js
+++ todoList/script.js
@@ -7,45 +7,53 @@ let botonAñadir = $('#botonAñadir');
 let formulario = $('#formulario');
 let mensajeError = $('#error');
 let tablaTareas = $('#tablaTareas');
+let tareaTexto = tarea.val().trim(); 
 
 const tarea = $('#inputTarea');
-
 
 function agregar() {
     formulario.on('submit', function(event) {
         event.preventDefault(); 
 
-        
         if (tarea.val().trim() === '') { 
             mensajeError.removeClass('d-none');  
         } else {
             mensajeError.addClass('d-none');
-            let tareaText = tarea.val().trim(); 
-            todas.push(tareaText); 
-            pendientes.push(tareaText);
+            todas.push(tareaTexto); 
+            pendientes.push(tareaTexto);
             input.val(''); 
-            actualizarTabla(tareaText);
+            actualizarTabla(tareaTexto);
         }
     });
 }
 
-
-function actualizarTabla(tareaText) {
-    
-    if (tareaText !== '') {
+function actualizarTablaTodasPendientes(tareaTexto) {
+    if (tareaTexto !== '') {
         let nuevaFila = `<tr>
-                            <td>${tareaText}</td>
+                            <td>${tareaTexto}</td>
                             <td class="d-flex justify-content-end">
-                                <button class=" ml-auto btn btn-success me-2">Completados</button>
-                                <button class=" ml-auto btn btn-warning me-2">Pendientes</button>
-                                <button class=" ml-auto btn btn-danger">Eliminar</button>
+                                <button class="ml-auto btn btn-success me-2">Completados</button>
+                                <button class="ml-auto btn btn-warning me-2">Editar</button>
+                                <button class="ml-auto btn btn-danger btnEliminar">Eliminar</button>
                             </td>
                         </tr>`; 
 
-        
         tablaTareas.append(nuevaFila);
     }
 }
 
 
+
+function eliminar() {
+    tablaTareas.on('click', '.btnEliminar', function(event) {
+        event.preventDefault();
+        $(this).closest('tr').remove(); 
+    });
+}
+
+eliminar();
 agregar();
+
+
+//Para lo de completar y demas recorrer el array de pendientes y todas y ponerla en la tabla y despues hacer
+//otra tabla para completadas. Hacer magia
