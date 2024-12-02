ebc95958cef1bae997e2dd0d3e70b64cbb23e2a4
dani123455 <147657091+dani123455@users.noreply.github.com>
2024-12-02 14:12:26 +0100

    AÑADIENDO ARCHIVOS

--- todoList/script.js
+++ todoList/script.js
@@ -7,68 +7,74 @@ let botonAñadir = $('#botonAñadir');
 let formulario = $('#formulario');
 let mensajeError = $('#error');
 let tablaTareas = $('#tablaTareas');
-let tablaCompletadas = $('#tablaCompletadas'); 
-let secciones = $('.secciones'); 
+let tablaCompletadas = $('#tablaCompletadas');
+let tablaPendientes = $('#tablaPendientes');
+let secciones = $('#secciones');
 
 function agregar() {
     formulario.on('submit', function(event) {
-        event.preventDefault(); 
+        event.preventDefault();
 
-        if (input.val().trim() === '') { 
-            mensajeError.removeClass('d-none');  
+        if (input.val().trim() === '') {
+            mensajeError.removeClass('d-none');
         } else {
             mensajeError.addClass('d-none');
-            let tareaTexto = input.val().trim(); 
-            todas.push(tareaTexto); 
+            let tareaTexto = input.val().trim();
+            todas.push(tareaTexto);
             pendientes.push(tareaTexto);
-            input.val(''); 
+            input.val('');
             actualizarTablaTodas();
         }
     });
 }
 
 function actualizarTablaTodas() {
-    tablaTareas.empty(); 
+    tablaTareas.empty();
     todas.forEach(function(tareaTexto) {
         if (tareaTexto !== '') {
-            let nuevaFila = `<tr>
-                                <td>${tareaTexto}</td>
-                                <td class="d-flex justify-content-end">
-                                    <button class="ml-auto btn btn-success me-2 btnCompletar">Completados</button>
-                                    <button class="ml-auto btn btn-warning me-2">Editar</button>
-                                    <button class="ml-auto btn btn-danger btnEliminar">Eliminar</button>
-                                </td>
-                            </tr>`; 
+            let nuevaFila = `
+                <tr>
+                    <td>${tareaTexto}</td>
+                    <td class="d-flex justify-content-end">
+                        <button class="ml-auto btn btn-success me-2 btnCompletar">Completar</button>
+                        <button class="ml-auto btn btn-warning me-2">Editar</button>
+                        <button class="ml-auto btn btn-danger btnEliminar">Eliminar</button>
+                    </td>
+                </tr>`;
             tablaTareas.append(nuevaFila);
         }
     });
 }
 
 function actualizarTablaCompletadas() {
-    tablaCompletadas.empty(); 
-    completadas.forEach(function(tareaCompletada) {
-        if (tareaCompletada !== '') {
-            let nuevaFila = `<tr>
-                                <td>${tareaCompletada}</td>
-                                <td class="d-flex justify-content-end">
-                                    <button class="ml-auto btn btn-warning me-2">Editar</button>
-                                    <button class="ml-auto btn btn-danger btnEliminar">Eliminar</button>
-                                </td>
-                            </tr>`; 
+    tablaCompletadas.empty();
+    if(tablaCompletadas.on('click','.btnSeccionCompletar', function(event){
+        event.preventDefault();
+        completadas.forEach(function(tareaCompletada) {
+            let nuevaFila = `
+                <tr>
+                    <td>${tareaCompletada}</td>
+                    <td class="d-flex justify-content-end">
+                        <button class="ml-auto btn btn-warning me-2">Editar</button>
+                        <button class="ml-auto btn btn-danger btnEliminar">Eliminar</button>
+                    </td>
+                </tr>`;
             tablaCompletadas.append(nuevaFila);
-        }
-    });
+        });
+    }));
+
 }
 
 function completarTarea() {
     tablaTareas.on('click', '.btnCompletar', function(event) {
         event.preventDefault();
-        let tareaTexto = $(this).closest('tr').find('td:first').text(); 
+        let tareaTexto = $(this).closest('tr').find('td:first').text();
+        
         if (!completadas.includes(tareaTexto)) {
             completadas.push(tareaTexto);
-            pendientes = pendientes.filter(tarea => tarea !== tareaTexto); 
-            actualizarTablaTodas(); 
-            actualizarTablaCompletadas(); 
+            pendientes = pendientes.filter(tarea => tarea !== tareaTexto);
+            actualizarTablaTodas();
+            actualizarTablaCompletadas();
         }
     });
 }
@@ -84,14 +90,32 @@ function eliminar() {
 
     tablaCompletadas.on('click', '.btnEliminar', function(event) {
         event.preventDefault();
-        let tareaTexto = $(this).closest('tr').find('td:first').text(); 
+        let tareaTexto = $(this).closest('tr').find('td:first').text();
         completadas = completadas.filter(tarea => tarea !== tareaTexto);
-        actualizarTablaCompletadas(); 
+        actualizarTablaCompletadas();
     });
 }
+
+function manejoSecciones() {
+    secciones.on('click', 'button', function() {
+        tablaTareas.hide();
+        tablaCompletadas.hide();
+        tablaPendientes.hide();
+
+        if ($(this).hasClass('btnSeccionTodas')) {
+            tablaTareas.show();
+        } else if ($(this).hasClass('btnSeccionCompletar')) {
+            tablaCompletadas.show();
+        } else if ($(this).hasClass('btnSeccionPendientes')) {
+            tablaPendientes.show();
+        }
+    });
+}
+
 
 eliminar();
 agregar();
 completarTarea();
+manejoSecciones();
 
-//Poner en el tbdoy d-none y cuando cambies de seccion se quite la lase de d-none para que salga 
+//HACER QUE SE VEA LAS TAREAS EN COMPLETADO Y PENDIENTES 
