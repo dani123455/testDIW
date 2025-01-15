aa9e03fd6f6400cae114bdef6d4f17524890ecaa
Daniel <daniel.mg1705@gmail.com>
2025-01-15 13:51:43 +0100

    continuando proyecto

--- WGAG/script.js
+++ WGAG/script.js
@@ -1,7 +1,13 @@
 const temas = document.getElementById('temas');
 const root = document.documentElement; 
+const botonFondo = document.getElementById('botonFondo');
+const containerFondo = document.getElementById('containerFondo');
 const click = document.getElementById('click');
 let contador = 0;
+let contador2 = 0;
+const progreso = document.getElementById('progreso');
+const progreso2 = document.getElementById('progreso%');
+
 
 
 
@@ -13,17 +19,54 @@ if (guardado) {
 
 // Función para alternar el tema
 temas.addEventListener('click', function() {
-    if (root.classList.contains('dark-theme')) {
-        root.classList.remove('dark-theme');
+    if (root.classList.contains('tema-oscuro')) {
+        root.classList.remove('tema-oscuro');
         localStorage.setItem('theme', ''); 
     } else {
-        root.classList.add('dark-theme');
-        localStorage.setItem('theme', 'dark-theme'); 
+        root.classList.add('tema-oscuro');
+        localStorage.setItem('theme', 'tema-oscuro'); 
     }
 });
+
+//Funcion para cambiar color del fondo 
+
+function colorRamdom() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    
+    containerFondo.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+}
+
+if (botonFondo) {
+    botonFondo.addEventListener('click', colorRamdom);
+}
+
+
+
 
 //contador de cliks
 click.addEventListener('click', function(){
     contador++;
     document.getElementById('contador').textContent = 'Clicks: ' + contador; 
 })
+
+
+//Barra progreso 
+
+window.addEventListener('scroll', function() {
+    let scroll = document.documentElement.scrollTop; 
+    let scrollAbajo = document.documentElement.scrollHeight; 
+
+    
+    if (scroll < scrollAbajo) {
+        if (contador2 < 100) { 
+            contador2++;
+            progreso.value = contador2; 
+            progreso2.innerHTML = contador2 + '%';
+        }
+    }
+});
+
+
+//Arreglar progreso añadir boton de ir arriba , dar bienvenida , leer pagina y aumentar texto 
